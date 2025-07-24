fbce855bcc0ef6347dc98518aaffa1b00d159afb
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-07-24 13:55:57 +0200

    changes

--- rotary_encoder.h
+++ rotary_encoder.h
@@ -9,14 +9,16 @@ typedef struct {
     rotary_encoder_interface_t interface;
 } rotary_encoder_t;
 
-rotary_encoder_err_t rotary_encoder_initialize(rotary_encoder_t* encoder,
-                                               rotary_encoder_config_t const* config,
-                                               rotary_encoder_interface_t const* interface);
+rotary_encoder_err_t rotary_encoder_initialize(
+    rotary_encoder_t* encoder,
+    rotary_encoder_config_t const* config,
+    rotary_encoder_interface_t const* interface);
 rotary_encoder_err_t rotary_encoder_deinitialize(rotary_encoder_t* encoder);
 
 rotary_encoder_err_t rotary_encoder_reset(rotary_encoder_t* encoder);
 
-rotary_encoder_err_t rotary_encoder_get_position(rotary_encoder_t* encoder, float32_t* position);
+rotary_encoder_err_t rotary_encoder_get_position(rotary_encoder_t* encoder,
+                                                 float32_t* position);
 rotary_encoder_err_t rotary_encoder_get_speed(rotary_encoder_t* encoder,
                                               float32_t* speed,
                                               float32_t delta_time);
